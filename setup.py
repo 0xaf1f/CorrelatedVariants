f906aa6b9572128fe4a1918e389ed9a256194ba6
Afif Elghraoui <afif@ghraoui.name>
2015-12-23 14:31:12 -0800

    Remove autogenerated files and add a setuptools build system

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'CorrelatedVariants',
+    version = '0.1.0',
+    author = 'Pacific Biosciences',
+    author_email = 'devnet@pacificbiosciences.com',
+    license = open('LICENSE.txt').read(),
+    packages = find_packages('.'),
+    package_dir = {'':'.'},
+    zip_safe = False,
+    scripts=[
+        'bin/correlatedVariants.py',
+        'bin/rareCaller.py'
+    ],
+    install_requires = [
+        'pbcore >= 0.2',
+        'numpy >= 1.6.0',
+        'scipy >= 0.9.0',
+        'h5py >= 1.3.0'
+    ]
+)
